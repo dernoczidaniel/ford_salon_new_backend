6b01f1526b11648a5425bde9a4825d6bbc89ad1f
Dernóczi Balázs <dernoczi.daniel@students.jedlik.eu>
2023-04-28 16:35:13 +0200

    comm

--- server.js
+++ server.js
@@ -58,60 +58,6 @@ app.use('/', usersRouter);
 app.use('/', orderRouter);
 
 
-// //Bejelenkezés
-// app.post('/api/login', (req, res) => {
-//   const email = req.body.email;
-//   const password = req.body.password;
-
-//   // Itt az adatok validálása
-//   if (email === 'user@example.com' && password === 'password') {
-//     res.json({ success: true });
-//   } else {
-//     res.json({ success: false });
-//   }
-// });
-// // /Bejelenkezés
-
-// //autó valasztas
-// app.post('/selected-car', (req, res) => {
-//   const selectedCar = req.body;
-//   // Itt feldolgozhatod a selectedCar adatokat, pl. elmentheted azokat adatbázisba
-//   console.log(selectedCar);
-//   res.status(200).send('OK');
-// }); 
-
-
-
-// //Registráció
-// app.post('/register', (req, res) => {
-//   const { name, email, password, city } = req.body;
-
-//   // Validate the data
-//   if (!name || !email || !password || !city) {
-//     return res.status(400).json({ error: 'All fields are required' });
-//   }
-
-//   // Check password strength
-//   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-//   if (!passwordRegex.test(password)) {
-//     return res.status(400).json({ error: 'Password is not strong enough' });
-//   }
-
-//   // If data is valid, store it in database or perform other actions
-//   // ...
-//   const cookieOptions = {
-//     httpOnly: true,
-//     secure: true,
-//     sameSite: 'strict'
-//   };
-//   // Set a cookie with a unique identifier or other data
-//   const userId = '12345'; // Replace with actual user ID or other data
-//   res.cookie('userId', userId);
-
-//   // Return a success response
-//   res.json({ message: 'Registration successful' });
-// });
-// //  /Registráció
 
 
 require('./routes/ford.route')(app);
